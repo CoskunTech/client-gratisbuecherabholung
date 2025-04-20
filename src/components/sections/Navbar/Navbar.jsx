b7b9d9dd83f73d064ef6b6d0e9d6c8373b0458c1
Abdullah Veysel Çoşkun <abdullahvcoskun@gmail.com>
2025-04-20 08:18:59 +0300

    update

--- src/components/sections/Navbar/Navbar.jsx
+++ src/components/sections/Navbar/Navbar.jsx
@@ -3,41 +3,46 @@ import { WhatsAppButton } from "../../comon";
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md">
-      <div className="container mx-auto px-4">
-        <div className="flex justify-between items-center h-16">
+      <div className="container px-4 mx-auto">
+        <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <div className="flex items-center">
+          <div className="flex items-center justify-center gap-2">
             <h1 className="text-xl font-bold">
               <span className="text-green-600">Gratis </span>
               <span className="text-gray-800">Bücher Abholung</span>
             </h1>
+            <img
+              src="./public/assets/logo.png"
+              alt="Gratis Bücher Abholung"
+              className="w-10 h-10"
+            />
           </div>
 
           {/* Navigation Links */}
-          <div className="hidden lg:flex items-center space-x-4">
+          <div className="items-center hidden space-x-4 lg:flex">
             <a
               href="#startseite"
-              className="text-gray-800 hover:text-green-600 transition-colors">
+              className="text-gray-800 transition-colors hover:text-green-600">
               Startseite
             </a>
             <a
               href="#hilfe"
-              className="text-gray-800 hover:text-green-600 transition-colors">
+              className="text-gray-800 transition-colors hover:text-green-600">
               So helfen wir Ihnen
             </a>
             <a
               href="#abholtermine"
-              className="text-gray-800 hover:text-green-600 transition-colors">
+              className="text-gray-800 transition-colors hover:text-green-600">
               Abholtermine
             </a>
             <a
               href="#ueberuns"
-              className="text-gray-800 hover:text-green-600 transition-colors">
+              className="text-gray-800 transition-colors hover:text-green-600">
               Über uns
             </a>
             <a
               href="#kontakt"
-              className="text-gray-800 hover:text-green-600 transition-colors">
+              className="text-gray-800 transition-colors hover:text-green-600">
               Kontakt
             </a>
             <WhatsAppButton />
